42493daee1e6480a592ba09773b5fe66b16d5a6c
MohitRATHEE <104315872+MohitRATHEE@users.noreply.github.com>
2022-04-24 22:06:00 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n1;
+printf("enter any number\n);
+scanf("%d",&n1);
+if(n1%2==0)
+{
+printf("the entered no is even %d");
+}
+else
+{
+printf("the entered no is odd %d");
+}
+return0;
+}
