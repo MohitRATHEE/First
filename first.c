9c70864d57ee5d85e3b9fd1fa52fbd9c49ac3e0e
MohitRATHEE <104315872+MohitRATHEE@users.noreply.github.com>
2022-04-24 22:22:27 +0530

    Update first.c

--- first.c
+++ first.c
@@ -6,11 +6,11 @@ printf("enter any number\n);
 scanf("%d",&n1);
 if(n1%2==0)
 {
-printf("the entered no is even %d");
+printf("the entered no is even %d",n1);
 }
 else
 {
-printf("the entered no is odd %d");
+printf("the entered no is odd %d",n1);
 }
 return0;
 }
